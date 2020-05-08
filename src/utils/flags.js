f44cd1be46a0e16024c9600d535a27c2d71ea654
Justin Pflughaupt <jpflughaupt@gmail.com>
2020-05-08 13:03:21 -0600

    Update flags.js
    add config fetched handler

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,11 +1,13 @@
 import Rox from 'rox-browser'
 
-export const Flags = {
-	sidebar: new Rox.Flag(false),
-	title: new Rox.Flag(false)
+export const configurationFetchedHandler = fetcherResults => {
+  if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
+    window.location.reload(false)
+  }
 };
 
 const options = {
+  configurationFetchedHandler: configurationFetchedHandler
 };
 
 Rox.register('default', Flags);
