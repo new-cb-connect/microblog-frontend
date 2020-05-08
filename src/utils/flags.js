c1441c023f90393a466aec47ea8c9a87a04a153e
Justin Pflughaupt <jpflughaupt@gmail.com>
2020-05-08 13:19:44 -0600

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,5 +1,10 @@
 import Rox from 'rox-browser'
 
+export const Flags = {
+  sidebar: new Rox.Flag(false),
+  title: new Rox.Flag(false)
+};
+
 export const configurationFetchedHandler = fetcherResults => {
   if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
     window.location.reload(false)
